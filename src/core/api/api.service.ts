1a5d08d77f21ac3cac50ec4e0355b833b88e5cef
Kyle <k00benne@gmail.com>
2020-04-10 01:23:36 +0000

    update api url

--- src/core/api/api.service.ts
+++ src/core/api/api.service.ts
@@ -2,7 +2,7 @@ import { App } from '../pulse';
 import { Log } from '../../utils';
 
 const ApiInstance = App.API({
-  baseURL: `http://localhost:3000/v1`,
+  baseURL: `https://dev-api.pebblo.org/v1`,
   options: {
     headers: {
       ['Content-Type']: 'application/json',
