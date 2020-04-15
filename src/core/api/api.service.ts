dc2becf3ecd83442d051592a652a9fe03e220cd1
Kyle <kek@voided.pw>
2020-04-15 12:45:17 -0400

    change landing page

--- src/core/api/api.service.ts
+++ src/core/api/api.service.ts
@@ -2,11 +2,10 @@ import { App } from '../pulse';
 import { Log } from '../../utils';
 
 const ApiInstance = App.API({
-  baseURL: `https://dev-api.pebblo.org/v1`,
+  baseURL: `https://devapi.pebblo.org/v1`,
   options: {
     headers: {
       ['Content-Type']: 'application/json',
-      ['bigTestE']: 'success'
     }
   }
 })
