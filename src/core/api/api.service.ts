1aec6550813e8994cf6cceeb8219d20fb1ee39ea
itsRems <contact@itsrems.com>
2020-04-15 17:34:01 +0200

    switched to dev api

--- src/core/api/api.service.ts
+++ src/core/api/api.service.ts
@@ -2,7 +2,7 @@ import { App } from '../pulse';
 import { Log } from '../../utils';
 
 const ApiInstance = App.API({
-  baseURL: `https://api.pebblo.org/v1`,
+  baseURL: `https://dev-api.pebblo.org/v1`,
   options: {
     headers: {
       ['Content-Type']: 'application/json',
