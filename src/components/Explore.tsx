d5c0d30e64f62784882eff88405b67cd76b22fc4
yourlocalAndre <132revenge132@gmail.com>
2020-04-19 19:53:46 +1200

    Updated explore

--- src/components/Explore.tsx
+++ src/components/Explore.tsx
@@ -5,8 +5,20 @@ const Explore = () => {
         <div className={styles.explore}>
             <div className={styles.explore_topBar}>
                 <form className={styles.explore_search}>
-                    <input type="text" name="" id=""/>
+                        <input
+                            type="text"
+                            name="explore_search"
+                            placeholder="Search"
+                        />
+                        <button
+                            type="submit"
+                        >GO</button>
                 </form>
+            </div>
+
+            <div className={styles.explore_content}>
+                <h1 className={styles.explore_title}>Trending</h1>
+                <h1 className={styles.explore_title}>Feed</h1>
             </div>
         </div>
     )
