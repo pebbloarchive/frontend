7ca57a156f98a9d138a9bed46c96d9a5f4a98e5e
yourlocalAndre <132revenge132@gmail.com>
2020-04-23 12:42:36 +1200

    created single post pages

---
+++ src/pages/u/post/userpost.tsx
@@ -0,0 +1,20 @@
+import Nav from '../../../components/Nav';
+import Userpost from '../../../components/Userpost';
+import Router from 'next/router'
+import { Account } from '../../../core';
+import { Log } from '../../../utils';
+import { usePulse } from 'pulse-framework';
+
+const Page = () => {
+  // const [followingPosts] = usePulse([PostCollection.getGroup('following')]);
+  // const [logged] = usePulse(Account.isUserLoggedIn);
+  // if (!logged && process.browser) Router.replace('/login');
+
+  return (
+    <>
+      <Nav />
+      <Userpost />
+    </>
+  )
+}
+export default Page;
