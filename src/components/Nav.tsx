5b5e0e6223e4dbd1e9d803ab6d9469020fab408c
yourlocalAndre <132revenge132@gmail.com>
2020-04-24 17:14:26 +1200

    updated navbar icons/design

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -10,11 +10,11 @@ const Nav = () => {
       </a>
       <div className={styles.navigation_content}>
         <div className={styles.navigation_links}>
-          <a href="profile" title="Profile"><img src="https://quak.ovh/44fw8G1.png" draggable={false} alt="" /></a>
+          <a href="profile" title="Profile" className={styles.navigation_profile}><img src="https://cdn.discordapp.com/avatars/215302985826304010/54c15631503fc334afae01bd91fe6c3a.png?size=1024" draggable={false} alt="" /></a>
           {/* <a href="library" title="Library"><img src="https://quak.ovh/9CTKBHB.png" alt="" /></a> */}
-          <a href="explore" title="Explore"><img src="https://cdn.discordapp.com/attachments/596156721928470547/701294239463637002/compass.png" draggable={false} alt="" /></a>
-          <a href="notifications" title="Notifications"><img src="https://cdn.discordapp.com/attachments/637859135970607131/702396304046817390/notifications.png" draggable={false} alt="" /></a>
-          <a href="messages" title="Messages"><img src="https://cdn.discordapp.com/attachments/596156721928470547/701295179297980466/chat-bubble.png" draggable={false} alt="" /></a> {/* https://quak.ovh/kGUKdDY.svg */}
+          <a href="explore" title="Explore"><img src="https://cdn.discordapp.com/attachments/596156721928470547/703106144591151215/search.png" draggable={false} alt="" /></a>
+          <a href="notifications" title="Notifications"><img src="https://cdn.discordapp.com/attachments/596156721928470547/703106874073022474/melting-heart.png" draggable={false} alt="" /></a>
+          <a href="messages" title="Messages"><img src="https://cdn.discordapp.com/attachments/596156721928470547/703107939925491752/speech-bubble.png" draggable={false} alt="" /></a> {/* https://quak.ovh/kGUKdDY.svg */}
         </div>
       </div>
 
