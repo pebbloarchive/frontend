759326b3e52237a18e219e81952277d7e5172967
yourlocalAndre <132revenge132@gmail.com>
2020-06-01 13:29:33 +1200

    updated profile feed

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -6,11 +6,11 @@ const Post = (props: PostInterface) => {
     <div className={styles.feed_post}>
       <div className={styles.feed_profile}>
         <a href="">
-<<<<<<< HEAD:src/components/Post.tsx
+{/* <<<<<<< HEAD:src/components/Post.tsx */}
           <img src="https://cdn.discordapp.com/avatars/215302985826304010/54c15631503fc334afae01bd91fe6c3a.png?size=1024" alt="" draggable={false} className={styles.feed_avatar} />
-=======
+{/* ======= */}
           <img src={props.author.avatar} alt="" draggable={false} className={styles.feed_avatar} />
->>>>>>> a7894d53d94a500cfb472f542d35c640f39ecd63:src/components/posts.tsx
+{/* >>>>>>> a7894d53d94a500cfb472f542d35c640f39ecd63:src/components/posts.tsx */}
           <div className={styles.feed_profile_info}>
             <h1 className={styles.feed_profile_username}> { props.author.username } </h1>
             <span className={styles.feed_profile_timeStamp}>{ props.created_at }</span>
