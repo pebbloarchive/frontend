77e6b7daf3fad98a598bcb070adb42e49b73cb48
yourlocalAndre <132revenge132@gmail.com>
2020-07-29 23:23:19 +1200

    toes

---
+++ src/pages/components/loader.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/components/whypebblo.module.css';
+import Loader from '../../components/Loader';
+
+const Page = () => {
+return (
+    <div>
+        <Loader />
+    </div>
+)
+}
+
+export default Page;
