22f2fc0fbf25449ca89e70ea3bbf3e7cda7fa9c6
yourlocalAndre <132revenge132@gmail.com>
2020-11-11 15:20:08 +1300

    features page 25% done

--- web/src/components/general/Features/Pebblo.tsx
+++ web/src/components/general/Features/Pebblo.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import React from 'react';
 import styles from '../../styles/landing.module.css'
 
 const Page = () => {
@@ -6,8 +8,36 @@ const Page = () => {
             <div className={styles.features}>
                 <div className={styles.about}>
 
-                    <h1 className={styles.features_title_center}>Who Are we?</h1>
-                    <p className={styles.features_message_center}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto consequatur, inventore distinctio, tempore ratione reiciendis voluptates libero iste deleniti laboriosam praesentium dolorem molestiae delectus quae, rerum sapiente mollitia tempora illum!</p>
+                    <div className={styles.about_landing}>
+                        <h1 className={styles.about_title}>
+                            <strong>Since October 2019 <br/>
+                            we have been creating <br/></strong>
+                            <span>Pebblo just for you</span>
+                        </h1>
+                        <Link href="/create"><a href="/create">Create an account</a></Link>
+                    </div>
+
+                    <div className={styles.about_social}>
+                        <img src="/images/svg/man-computer-desk.svg" alt="" className={styles.about_social_image}/>
+                        <div className={styles.about_social_title}>
+                            <h1>Share your stories with friends</h1>
+                            <p>Create a post on your timeline, follow and interact with your friends in a simple way.</p>
+                        </div>
+                    </div>
+
+                    <div className={styles.about_business}>
+                        <div className={styles.about_business_title}>
+                            <h1>We're bringing you simple ways to <span>grow your business</span> and <span>get your business found</span> with SEO</h1>
+                            <Link href="/features/business"><a href="/features/business">Learn more</a></Link>
+                        </div>
+                    </div>
+
+                    <div className={styles.about_creators}>
+                        <div className={styles.about_creators_title}>
+                            <h1>Helping you grow your community and pump your posts using our content creator system</h1>
+                            <Link href="/features/content-creators"><a href="/features/content-creators">Learn more</a></Link>
+                        </div>
+                    </div>
 
                 </div>
             </div>
